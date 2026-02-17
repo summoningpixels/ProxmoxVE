6931f3c0e68afc6a50602a60c0d7e234b15e3b29
summoningpixels <summoningpixels@gmail.com>
2026-02-17 15:16:42 +0200

    sed-ing the right stuff

--- install/arcane-install.sh
+++ install/arcane-install.sh
@@ -38,13 +38,15 @@ curl -fsSL "https://raw.githubusercontent.com/getarcaneapp/arcane/refs/heads/mai
 
 msg_info "Setup Arcane Environment"
 curl -fsSL "https://raw.githubusercontent.com/getarcaneapp/arcane/refs/heads/main/.env.example" -o "/opt/arcane/.env"
-APP_URL="localhost:3552"
+APP_URL="http://localhost:3552"
 ENCRYPTION_KEY=$(openssl rand -base64 32 | tr -d '/+=')
 JWT_SECRET=$(openssl rand -base64 24 | tr -d '/+=')
 
+sed -i "s/^ENCRYPTION_KEY=.*/ENCRYPTION_KEY=${ENCRYPTION_KEY}/" /opt/arcane/compose.yaml
+sed -i "s/^JWT_SECRET=.*/JWT_SECRET=${JWT_SECRET}/" /opt/arcane/compose.yaml
 sed -i "s/^APP_URL=.*/APP_URL=${APP_URL}/" /opt/arcane/.env
-sed -i "s/^ENCRYPTION_KEY=.*/ENCRYPTION_KEY=${ENCRYPTION_KEY}/" /opt/arcane/.env
-sed -i "s/^JWT_SECRET=.*/JWT_SECRET=${JWT_SECRET}/" /opt/arcane/.env
+sed -i "/^s/^ENCRYPTION_KEY/s/^/#/" /opt/arcane/.env
+sed -i "/^s/^JWT_SECRET/s/^/#/" /opt/arcane/.env
 
 msg_ok "Setup Arcane Environment"
 
